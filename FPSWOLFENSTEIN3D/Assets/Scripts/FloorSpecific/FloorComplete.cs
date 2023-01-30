37735ef8ed00ecc4d7327f1a42fdfafc68049ba3
Tankiet <119557091+Kietzzz@users.noreply.github.com>
2023-01-30 10:28:33 +0700

    upd

---
+++ FPSWOLFENSTEIN3D/Assets/Scripts/FloorSpecific/FloorComplete.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FloorComplete : MonoBehaviour
+{
+    public GameObject fadeOut;
+    public GameObject completePanel;
+    public GameObject thePlayer;
+    public GameObject floorTimer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        floorTimer.SetActive(false);
+        StartCoroutine(CompletedFloor());
+        thePlayer.GetComponent<FirstPersonMovement>().enabled = false;
+        thePlayer.GetComponent<Jump>().enabled = false;
+        thePlayer.GetComponent<Crouch>().enabled = false;
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+    }
+
+    IEnumerator CompletedFloor()
+    {
+        fadeOut.SetActive(true);
+        GlobalComplete.nextFloor += 1;
+        PlayerPrefs.SetInt("SceneToLoad", GlobalComplete.nextFloor);
+        PlayerPrefs.SetInt("LivesSaved", GlobalLife.lifeValue);
+        PlayerPrefs.SetInt("ScoreSaved", GlobalScore.scoreValue);
+        PlayerPrefs.SetInt("AmmoSaved", GlobalAmmo.handgunAmmo);
+
+
+
+
+        yield return new WaitForSeconds(2);
+        completePanel.SetActive(true);
+        yield return new WaitForSeconds(15);
+        GlobalScore.scoreValue = 0;
+        GlobalComplete.enemyCount = 0;
+        GlobalComplete.treasureCount = 0;
+        
+        SceneManager.LoadScene(GlobalComplete.nextFloor);
+    }
+}
